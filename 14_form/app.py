7def77440f92c2d5aad49cad526b0e0e438ff586
sging04 <sging2356@gmail.com>
2021-10-14 22:26:53 -0400

    14_form

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
+# Sports! - Sean Ging and Deven Maheshwari
 # SoftDev
-# Oct 2021
+# K14 - Form and Function/Intro to Flask/App to echo input from user.
+# Oct 14 2021
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -23,23 +24,23 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 '''
 
 @app.route("/") #, methods=['GET', 'POST'])
-def disp_loginpage():
-    print("\n\n\n")
+def disp_loginpage(): # Display. Prediction: Will work, login.html is in templates directory.
+    print("\n\n\n") # New line. HTML uses <br> tags
     print("***DIAG: this Flask obj ***")
-    print(app)
+    print(app) # Prints line 13
     print("***DIAG: request obj ***")
-    print(request)
+    print(request) # From form submission
     print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print(request.args) # Inputs from user
+    #print("***DIAG: request.args['username']  ***") # Test by uncommenting line by line and seeing outputs in terminal and app. Prediction: Will work, just printing to termianl.
+    #print(request.args['username']) # Test by uncommenting line by line and seeing outputs in terminal and app. # Prediction: Depends on user input
     print("***DIAG: request.headers ***")
     print(request.headers)
     return render_template( 'login.html' )
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
-def authenticate():
+def authenticate(): # Manipulation of form data. Prediction: Will work - does not reference external files.
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app)
@@ -47,14 +48,17 @@ def authenticate():
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print("***DIAG: request.args['username']  ***") # Test by uncommenting line by line and seeing outputs in terminal and app. Prediction: Will work, just printing to termianl.
+    #print(request.args['username']) # Test by uncommenting line by line and seeing outputs in terminal and app. Prediction: Depends on user input
     print("***DIAG: request.headers ***")
-    print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
+    print(request.headers) # Headers is attribute of request
+    if (request.method == "POST"):
+        return render_template( 'response.html', name = request.args['username'] )
+    return render_template( 'response.html' )  #response to a form submission
 
 
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True
+    app.run()
