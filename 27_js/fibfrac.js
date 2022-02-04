026148358f7515ef2f0ced18ffe074749e33f1d7
sging04 <67232054+sging04@users.noreply.github.com>
2022-02-04 08:35:46 -0500

    Update fibfrac.js

--- 27_js/fibfrac.js
+++ 27_js/fibfrac.js
@@ -6,16 +6,16 @@
 
 
 var fact = function(n){
-  if(<=n 1)
+  if(n <= 1)
   1
-  (* n(fact(- n 1)))
-}
+  (n * (fact(- n 1)))
+};
 
 var fib = function(n){
-  if(<= n 1)
+  if(n<=  1)
   n
-  (+ (fib(- n 1)) (fib(- n 2)))
-}
+  ((fib(n - 1)) + (fib(n - 2)))
+};
 
 
-print(fib(2))
+print(fib(2));
