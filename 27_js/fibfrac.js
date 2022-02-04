d6e0305e391350b34911c44bd8c6b83cbf455e00
sging04 <67232054+sging04@users.noreply.github.com>
2022-02-04 08:33:59 -0500

    Create fibfrac.js

---
+++ 27_js/fibfrac.js
@@ -0,0 +1,21 @@
+// Team Blue Duck: Zhao Yu Lin, Sean Ging, Patrick Ging
+// SoftDev
+// K27; Basic functions in JavaScript
+// 2022-02-04
+
+
+
+var fact = function(n){
+  if(<=n 1)
+  1
+  (* n(fact(- n 1)))
+}
+
+var fib = function(n){
+  if(<= n 1)
+  n
+  (+ (fib(- n 1)) (fib(- n 2)))
+}
+
+
+print(fib(2))
