fed7a1b9149a75589c579b9f357681bb7495e1cb
sging04 <67232054+sging04@users.noreply.github.com>
2022-02-15 08:39:38 -0500

    animate

---
+++ 31/animate.js
@@ -0,0 +1,22 @@
+var c = document.getElementById("slate");
+var stopButton = document.getElementById("stopButton");
+var startButton = document.getElementById("startButton");
+
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "blue";
+
+function clear(e) {
+  ctx.clearRect(0,0,c.width, c.height);
+}
+
+var radius = 0;
+var growing = true;
+
+function drawDot(e) {
+  clear(e);
+
+  ctx.beginPath();
+  ctx.arc(c.width / 2, c.height / 2, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+}
