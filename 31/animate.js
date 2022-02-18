7924050e1aca3de02c0efcfda2c4e296039e2c13
sging04 <67232054+sging04@users.noreply.github.com>
2022-02-18 08:31:47 -0500

    Update animate.js

--- 31/animate.js
+++ 31/animate.js
@@ -1,3 +1,12 @@
+//Team Blue Duck: Sean Ging, Patrick Ging
+//Softdev
+//K31
+//2022-02-16
+//time spent: 1hr
+
+
+
+
 var c = document.getElementById("slate");
 var stopButton = document.getElementById("stopButton");
 var startButton = document.getElementById("startButton");
@@ -12,6 +21,7 @@ function clear(e) {
 
 var radius = 10;
 var growing = true;
+var rID= null;
 var drawDot = (e) => {
   clear(e);
 
@@ -21,16 +31,41 @@ var drawDot = (e) => {
   ctx.arc(c.width / 2, c.height / 2, radius, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
-  radius += .5;
-  requestID=  window.requestAnimationFrame(drawDot);
+
+  if (radius >= 300) {
+    growing = false;
+  }
+  else if (radius == 1) {
+  	growing = true;
+  }
+  
+  
+  
+  if (growing){
+    radius += 1.5;
+  }
+  else {
+    radius -= 1.5;
+  }
+
+  rID =  window.requestAnimationFrame(drawDot);
+
 
   }
-var stopIt = (rID) => {
+var stopIt = () => {
   window.cancelAnimationFrame(rID);
 }
 
-function grow(e){
-  ctx.beginPath();
-}
+
 document.getElementById("startButton").addEventListener("click", drawDot);
 document.getElementById("stopButton").addEventListener("click",  stopIt);
+
+
+
+
+
+
+
+
+
+
