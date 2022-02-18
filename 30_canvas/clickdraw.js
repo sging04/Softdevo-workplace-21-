cf91ab719052060a56dc37c6b0ad39aa5f9d6ead
sging04 <67232054+sging04@users.noreply.github.com>
2022-02-17 22:20:13 -0500

    Update clickdraw.js

--- 30_canvas/clickdraw.js
+++ 30_canvas/clickdraw.js
@@ -6,38 +6,61 @@
 
 var c = document.getElementById('slate');
 
-var ctx = c.getContext("2d");
-
+var ctx = c.getContext("2d"); //2D
+//global state var
 var mode = "rect";
 
 // e means event
-function toggleMode(e) {
+var toggleMode = (e) => {
   console.log("toggleing..............");
-
+  if(mode === "rect"){
+    mode= "arc";
+  }
+  else{
+    mode= "rect";
+  }
 }
 
 
-function drawRect(e) {
-  var
+function drawRect = (e) =>{
   console.log("draw rect");
+  mouseX= e.offsetX; //position of mouse
+  mouseY= e.offsetY; //position
+  ctx.fillRect(mouseX, mouseY, 50, 100);
+  console.log("mouseclick at ", mouseX, mouseY);
 }
 
-function drawCircle(e) {
+function drawCircle = (e) => {
   console.log("drawCircle");
+  mouseX= e.offsetX;
+  mouseY= e.offsetY;
+  ctx.beginPath(); //resets each time
+  ctx.arc(mouseX, mouseY, 50, 0, Math.PI);
+  ctx.fill();
+  console.log("mouseclick at ", mouseX, mouseY);
 }
 
 
-function draw(e) {
-    e.
+function draw = (e) =>{
+  console.log("drawing");
+  if(mode=== "rect"){
+    drawRect(e);
+  }
+  else{
+    drawCircle(e);
+  }
 }
 
 
-function wipeCanvas(e) {
-
+function wipeCanvas = (e) =>{
+  ctx.clearRect(0,0, c.width, c.heigth); //makes all of canvas blank
 }
 
 c.addEventListener("click", draw);
-var bToggler = document.getElementById("buttonToggle");
-bToggler.addEventListener("click", draw);
+
+
 var clearB = document.getElementById("buttonClear");
-clearB.addEventListener("click", );
+clearB.addEventListener("click, wipeCanvas);
+
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
